d7007b052b176fdd966a3c1b661becdd8dd4be25
CrisKoch <cristinak.iftm@gmail.com>
2020-05-06 22:13:57 -0300

    Relatorio gerado

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -11,6 +11,9 @@ public class Programa {
 		
 		char sexo;
 		int cervejas, refri, espetos;
+		double consumo, couvert, ingresso, total;
+		
+		// 1 Leitura dos dados
 		
 		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -21,9 +24,42 @@ public class Programa {
 		System.out.print("Quantidade de espetinhos: ");
 		espetos =sc.nextInt();
 		
+		// 2 Calculos
 		
+		if (sexo == 'F' || sexo == 'f') {
+			ingresso = 8.00;
+		
+		}
+		else {
+			ingresso = 10.00;
+		}
+		
+		consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
+		
+		if (consumo > 30) {
+			couvert = 0.0;
+		}
+		else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+		
+		// 3 Relatorio
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if(couvert == 0) {
+			System.out.println("Isento de Couvert");
+		}
+		else {
+			System.out.printf("Couvert = R$ %.2fn", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc. close();
 	}
-
 }
